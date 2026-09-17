4fb48986b3454a90fc21724ac45d5a11f5207397
samiul-hasan-sysforge <samiulh085@gmail.com>
2026-09-17 17:44:27 +0600

    first commit2

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { AllMovies } from "../utils/FetchMovies";
-
+import MovieCard from "../components/MovieCard";
 
 const Movies = () => {
-    const [allMovies, setAllMovies] = useState([])
+    const [allMovies, setAllMovies] = useState([]);
 
     useEffect(() => {
         const getData = async () => {
@@ -12,12 +12,16 @@ const Movies = () => {
         };
         getData();
     }, []);
-    console.log(allMovies)
-
 
     return (
-        <div>
-            Movies
+        <div className="min-h-screen bg-gray-900 py-10 px-6">
+            <div className="max-w-7xl mx-auto">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                    {allMovies.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
